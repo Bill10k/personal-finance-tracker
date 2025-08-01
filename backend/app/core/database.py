f5206d7f0d1ce0b7f28f83a52c76aceb7decc619
EnmeryLives <146829459+EnmeryLives@users.noreply.github.com>
2025-08-01 13:06:49 +0000

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from typing import Dict
 
-DATABASE_URL = "sqlite:///./finance.db"  # Later: PostgreSQL URI
+DATABASE_URL = "postgresql://postgres:SQL@2025@localhost:5432/budget_app"  # Later: PostgreSQL URI
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}  # SQLite-specific
@@ -18,5 +18,6 @@ Base = declarative_base()
 users_db: Dict[str, dict] = {}
 accounts_db: Dict[str, dict] = {}
 categories_db: Dict[str, dict] = {}
+savings_goal_db: Dict[str, dict] = {}
 transactions_db: Dict[str, dict] = {}
 budgets_db: Dict[str, dict] = {}
